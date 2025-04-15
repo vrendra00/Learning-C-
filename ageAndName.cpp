36d824853a610e1a82a257ecdf9c7aeab85e1798
Virendra Vishwakarma <virendravishwakarma261@gmail.com>
2025-04-15 22:58:35 +0530

    add this simpl c++ programs

---
+++ ageAndName.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<conio.h>   
+using namespace std;
+int main()
+{
+    int age,  name;
+    cout<<"Enter your age \n"<<endl;
+    cout<<"Enter your  name\n"<<endl;    
+    cin>>age; cin>>name;
+    cout<<"Your age is"<<age<<endl;
+return 0;
+}
---
+++ twoIntegers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+int a,b,c;
+cout<<"Enter the values of a and b\n"<<endl;
+cin>>a>>b;
+c=a+b;
+cout<<"The sum of two integers is:\n"<<c<<endl;
+}
