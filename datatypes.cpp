f7770a1c26f78186547e47954662bb928b920cba
Virendra Vishwakarma <virendravishwakarma261@gmail.com>
2025-04-18 11:22:50 +0530

    add datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    cin>>i>>l>>c>>f>>d;
+cout<<i<<endl;
+cout<<l<<endl;
+cout<<c<<endl;
+cout<<fixed<<setprecision(3)<<endl;
+cout<<fixed<<setprecision(9)<<endl;
+return 0;
+}
