b51e493e3877fa4325134542bd3aa19da372aa05
Virendra Vishwakarma <virendravishwakarma261@gmail.com>
2025-04-20 18:17:20 +0530

    updated repo

---
+++ Basics/char_name.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    char name[30];
+   // cout<<"Enter Your Name : \n"<<endl;
+    cin>>name;
+    cout<<"Your Name is: \n"<<name;
+    return 0;
+}
---
+++ Basics/variables.cpp
@@ -0,0 +1,8 @@
+//ADDING TWO INTEGERS 
+#include<iostream>
+using namespace std;
+int main() {
+int a=7, b=9; int c;
+cout<<(c=a+b)<<endl;
+return 0;
+}
